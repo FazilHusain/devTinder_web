c6739109b077066ce9b5f7101cd1f939c19d5794
FazilHusain <fhmansoori99@gmail.com>
2025-03-15 16:36:37 +0530

    Routing

---
+++ src/Body.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import NavBar from './NavBar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+const Body = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Body
