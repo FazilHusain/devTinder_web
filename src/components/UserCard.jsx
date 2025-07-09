6554a05eef3ba474e14ea6ba91aed4bdc8642542
FazilHusain <fhmansoori99@gmail.com>
2025-07-09 14:23:15 +0530

    changes

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
-import React from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { removeFeed } from "../utils/feedSlice";
 
+// eslint-disable-next-line react/prop-types
 const UserCard = ({ user }) => {
-  const {_id, firstName, lastName, photoUrl, age, gender, about } = user;
+  // eslint-disable-next-line react/prop-types
+  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
   const dispatch = useDispatch();
 
   const handleSendRequest = async (status, userId) => {
@@ -31,8 +32,18 @@ const UserCard = ({ user }) => {
         {age && gender && <p>{age + " " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary" onClick={() => handleSendRequest("ignored", _id)} >Ignore</button>
-          <button className="btn btn-secondary" onClick={() => handleSendRequest("interested", _id)}>Interested</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
